ea8f3150ac48b73f066c6599f51ff3f5b5d050c9
Alan Christie <alan.christie@matildapeak.com>
2018-01-23 09:41:17 +0000

    - Remainign test migrated to PipelineTester

--- test-rdkit.sh
+++ test-rdkit.sh
@@ -74,40 +74,40 @@ msg_file_notCreated="\n========================== FILE NOT CREATED =============
 #    echo -e $msg_file_notCreated
 #fi
 
-echo "Testing screen_multi.py reading taget form sdf file, query as json file and writing to STDOUT"
-gunzip -c data/dhfr_3d.sdf.gz | python src/python/pipelines/rdkit/screen_multi.py\
-  -if sdf --qjson data/nci100.data.gz --simmin 0.55 > /dev/null || echo -e $msg_fail
+#echo "Testing screen_multi.py reading taget form sdf file, query as json file and writing to STDOUT"
+#gunzip -c data/dhfr_3d.sdf.gz | python src/python/pipelines/rdkit/screen_multi.py\
+#  -if sdf --qjson data/nci100.data.gz --simmin 0.55 > /dev/null || echo -e $msg_fail
 
-echo "Testing conformers.py reading from STDIN and writing to STDOUT"
-gunzip -c data/Kinase_inhibs.sdf.gz | python src/python/pipelines/rdkit/conformers.py -n 2 -if sdf > /dev/null || echo -e $msg_fail
+#echo "Testing conformers.py reading from STDIN and writing to STDOUT"
+#gunzip -c data/Kinase_inhibs.sdf.gz | python src/python/pipelines/rdkit/conformers.py -n 2 -if sdf > /dev/null || echo -e $msg_fail
 
-echo "Testing constrained_conf_gen.py reading from STDIN and writing to STDOUT"
-python src/python/pipelines/rdkit/constrained_conf_gen.py -n 2 -i data/XChemReactionMaker1.sdf.gz -r data/ref_mol.sdf.gz -o tmp/constrained_conf_gen.sdf.gz || echo -e $msg_fail
+#echo "Testing constrained_conf_gen.py reading from STDIN and writing to STDOUT"
+#python src/python/pipelines/rdkit/constrained_conf_gen.py -n 2 -i data/XChemReactionMaker1.sdf.gz -r data/ref_mol.sdf.gz -o tmp/constrained_conf_gen.sdf.gz || echo -e $msg_fail
 
-echo "Testing conformers.py with clustering reading from STDIN and writing to STDOUT"
-gunzip -c data/Kinase_inhibs.sdf.gz | python src/python/pipelines/rdkit/conformers.py -n 2 -c RMSD -if sdf > /dev/null || echo -e $msg_fail
+#echo "Testing conformers.py with clustering reading from STDIN and writing to STDOUT"
+#gunzip -c data/Kinase_inhibs.sdf.gz | python src/python/pipelines/rdkit/conformers.py -n 2 -c RMSD -if sdf > /dev/null || echo -e $msg_fail
 
-echo "Testing o3dAlign.py reading from STDIN and writing to STDOUT"
-gunzip -c data/Kinase_inhibs.sdf.gz | python src/python/pipelines/rdkit/o3dAlign.py\
-  data/pyrimethamine.mol -n 2 -t 10 -if sdf > /dev/null || echo -e $msg_fail
+#echo "Testing o3dAlign.py reading from STDIN and writing to STDOUT"
+#gunzip -c data/Kinase_inhibs.sdf.gz | python src/python/pipelines/rdkit/o3dAlign.py\
+#  data/pyrimethamine.mol -n 2 -t 10 -if sdf > /dev/null || echo -e $msg_fail
 
 echo "Testing filter.py reading from STDIN and writing to STDOUT"
 gunzip -c data/Kinase_inhibs.sdf.gz | python src/python/pipelines/rdkit/filter.py --hacmin 25 --hacmax 30 -if sdf > /dev/null || echo -e $msg_fail
 
-echo "Testing cluster_butina.py reading from STDIN and writing to STDOUT"
-gunzip -c data/Kinase_inhibs.sdf.gz | python src/python/pipelines/rdkit/cluster_butina.py -t 0.6 -if sdf > /dev/null || echo -e $msg_fail
+#echo "Testing cluster_butina.py reading from STDIN and writing to STDOUT"
+#gunzip -c data/Kinase_inhibs.sdf.gz | python src/python/pipelines/rdkit/cluster_butina.py -t 0.6 -if sdf > /dev/null || echo -e $msg_fail
 
-echo "Testing cluster_butina_matrix.py reading from STDIN and writing TSV to STDOUT"
-gunzip -c data/Kinase_inhibs.sdf.gz | src/python/pipelines/rdkit/cluster_butina_matrix.py -t 0.6 -if sdf -of tsv > /dev/null || echo -e $msg_fail
+#echo "Testing cluster_butina_matrix.py reading from STDIN and writing TSV to STDOUT"
+#gunzip -c data/Kinase_inhibs.sdf.gz | src/python/pipelines/rdkit/cluster_butina_matrix.py -t 0.6 -if sdf -of tsv > /dev/null || echo -e $msg_fail
 
-echo "Testing cluster_butina_matrix.py reading from STDIN and writing JSON to STDOUT"
-gunzip -c data/Kinase_inhibs.sdf.gz | src/python/pipelines/rdkit/cluster_butina_matrix.py -t 0.6 -if sdf -of json > /dev/null || echo -e $msg_fail
+#echo "Testing cluster_butina_matrix.py reading from STDIN and writing JSON to STDOUT"
+#gunzip -c data/Kinase_inhibs.sdf.gz | src/python/pipelines/rdkit/cluster_butina_matrix.py -t 0.6 -if sdf -of json > /dev/null || echo -e $msg_fail
 
-echo "Testing rxn_smarts_filter.py reading from STDIN and writing to files using SDF"
-gunzip -c data/Kinase_inhibs.sdf.gz | python src/python/pipelines/rdkit/rxn_smarts_filter.py -if sdf -o tmp/rxn_smarts_filter1 || echo -e $msg_fail
+#echo "Testing rxn_smarts_filter.py reading from STDIN and writing to files using SDF"
+#gunzip -c data/Kinase_inhibs.sdf.gz | python src/python/pipelines/rdkit/rxn_smarts_filter.py -if sdf -o tmp/rxn_smarts_filter1 || echo -e $msg_fail
 
-echo "Testing rxn_smarts_filter.py reading from STDIN and writing to files using JSON"
-gunzip -c data/nci100.data.gz | python src/python/pipelines/rdkit/rxn_smarts_filter.py -if json -o tmp/rxn_smarts_filter2 -of json --meta --thin || echo -e $msg_fail
+#echo "Testing rxn_smarts_filter.py reading from STDIN and writing to files using JSON"
+#gunzip -c data/nci100.data.gz | python src/python/pipelines/rdkit/rxn_smarts_filter.py -if json -o tmp/rxn_smarts_filter2 -of json --meta --thin || echo -e $msg_fail
 
 
 #echo "Testing rxn_smarts_filter.py reading from sd file and writing to multiple files"
