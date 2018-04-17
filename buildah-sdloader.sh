92ac517b93d4ce6688867a623b0da2ec41177301
Alan Christie <alan.christie@matildapeak.com>
2018-04-17 13:03:58 +0100

    - Some fixes for buildah file

--- buildah-sdloader.sh
+++ buildah-sdloader.sh
@@ -1,5 +1,8 @@
 #!/bin/bash -x
 
+# Just echo/log the Buildah version we have...
+buildah version
+
 # An image to populate a Service Descriptor destination directory
 # (SD_DST), which is normally mounted when the image is run,
 # with built-in Service Descriptor files from a source directory (SD_SRC).
@@ -9,7 +12,7 @@ SD_DST=/sd-dst
 
 CTR=`buildah from busybox`
 
-buildah config --author 'Tim Dudgeon <tdudgeon@informaticsmatters.com>' $CTR
+buildah config --author "Tim Dudgeon <tdudgeon@informaticsmatters.com>" $CTR
 buildah config --env SD_SRC=/sd-src $CTR
 buildah config --env SD_DST=/sd-dst $CTR
 
@@ -21,6 +24,6 @@ buildah copy $CTR src/nextflow/ ${SD_SRC}/nextflow/
 buildah run $CTR -- rm -f `find ${SD_SRC} -type f -not -name "*.json" -not -name "*.yml" -not -name "*.yaml"`
 
 # On execution copy files from source to destination...
-buildah config $CTR --cmd cp -R ${SD_SRC}/* ${SD_DST}
+buildah config --cmd "cp -R ${SD_SRC}/* ${SD_DST}" $CTR
 
 buildah unmount $CTR
