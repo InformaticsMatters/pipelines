26ebc6db991d69744b953fc94165337f92fd2c66
Alan Christie <alan.christie@matildapeak.com>
2018-06-21 10:45:59 +0100

    - Now logs in and moves to the required project

--- deploy.sh
+++ deploy.sh
@@ -3,10 +3,12 @@
 # A simple deployment script for OpenShift.
 # It is assumed that your OpenShift Squonk application has been deployed
 # and that you are running this from the Squonk project/namespace
-# and have logged in as an appropriate user.
+# and have logged in as an appropriate user and have run `source setenv.sh`
 
 set -e pipefail
 
+oc login -u $OC_ADMIN
+oc project $OC_PROJECT
 oc create -f post-service-descriptors.yaml
 
 echo "Squonk pipelines deployment is underway..."
